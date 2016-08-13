67be7c6ab0f114767cba1ae6e39456fb2c262315
Charles Sipe <csipe84@gmail.com>
2016-08-13 07:43:20 -0700

    Push survey form data to Firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,55 +11,21 @@
 angular.module('angularAppApp')
   .controller('MainCtrl', function ($scope, $http, current, places, $firebaseObject) {  
 
-    // See https://firebase.google.com/docs/web/setup#project_setup for how to
-    // auto-generate this config
+    /*
+    // Firebase config
     var config = {
       apiKey: "apiKey",
       authDomain: "projectId.firebaseapp.com",
       databaseURL: "https://survey-pop.firebaseio.com/"
     };
 
+    // Initialize Firebase
     firebase.initializeApp(config);
 
     var surveyResponses = firebase.database().ref();
-
-    var businessProfile = firebase.database().ref("businessProfile/signature-pointe"); 
+    */
 
     /*
-    $scope.submitSurvey = function(){
-      var question1 = $scope.question1;
-      var question2 = $scope.question2;
-      var question3 = $scope.question3;
-
-      surveyResponses.push({ 
-        'question1': question1, 
-        'question2': question2,
-        'question3': question3 
-      });
-
-      console.log("survey submitted")
-    } */
-    
-
-    $scope.submitAnswers = function(){
-      businessProfile.push({
-        'name': "Signature Pointe",
-        'code': "12345",
-        'question1': "It was awesome",
-        'question2': "I will be back",
-        'question3': "I highly recommend this business",
-        'firstName': "First Name",
-        'lastName': "Last Name",
-        'address': "1234 Madison Ave. Newyork, NY 56123"
-
-      })
-
-      console.log("The first answer: " + $scope.question1);
-      console.log("The second answer: " + $scope.question2);
-      console.log("First name: " + $scope.firstName);
-
-    }
-
     // Get the business name from Firebase
     $scope.getBusinessName = function(){
       surveyResponses.once("value")
@@ -73,26 +39,35 @@ angular.module('angularAppApp')
         
     }
     var currentBusinessName = $scope.getBusinessName();
-    console.log(currentBusinessName);
+    console.log("The current business name is:" + currentBusinessName); // undefined
 
 
+    // get the survey code from 
+
+    $scope.getSurveyCode = function(){
+      surveyResponses.once("value")
+        .then(function(snapshot) {
+          //var firstName = snapshot.child("businessProfile/signature-pointe").val(); // "Ada"
+          var currentSurveyCode = snapshot.child("businessProfile").child("signature-pointe").child("surveyCode").val(); 
+
+          console.log("The current survey code is: "+ currentSurveyCode);
+          return currentSurveyCode;
+        });
+        
+    }
+    $scope.currentCode = $scope.getSurveyCode();
+
+    */
 
 
     //var ref = new Firebase("https://survey-pop.firebaseio.com/");
 
     // download the data into a local object
-    $scope.data = surveyResponses;
+    //$scope.data = surveyResponses;
 
 
 
     $scope.current = current.query();
-
-    setTimeout(function() { /*something*/ 
-      console.log($scope.data);
-    }, 1000);
-    
-
-    //$scope.yelp = yelp.query();
 
     $scope.places = places.query();
 
@@ -105,25 +80,6 @@ angular.module('angularAppApp')
 
   };
 
-    /*
-    // Request API access: http://www.yelp.com/developers/getting_started/api_access
-    var Yelp = require('yelp');
-
-    var yelp = new Yelp({
-      consumer_key: 'wfmrdaQihSpXxYS1YTwG1A',
-      consumer_secret: '4pZdOhA4K1uyko7VhFHtly_N47k',
-      token: 'G68wajA06Nij1dlq1coGDigKrc0TuK2M',
-      token_secret: 'QtfGTrldDslaqGVZszWVxrQ-e1o',
-    });
-
-    // See http://www.yelp.com/developers/documentation/v2/search_api
-    yelp.search({ term: 'food', location: 'Montreal' })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-    */
 
   });
+
--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -8,10 +8,47 @@
  * Controller of the angularAppApp
  */
 angular.module('angularAppApp')
-  .controller('ReviewCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ReviewCtrl', function ($scope, $firebaseObject) {
+    
+    // Firebase config
+    var config = {
+      apiKey: "apiKey",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://survey-pop.firebaseio.com/"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(config);
+
+    // cache Firebase reference
+    var surveyResponses = firebase.database().ref();
+
+    $scope.getFormValues = function(){
+
+      surveyResponses.child("businessProfile").child("signature-pointe").push({
+        'name': "Signature Pointe",
+        'code': $scope.surveyCode,
+        'question1': $scope.question1,
+        'question2': $scope.question2,
+        'question3': $scope.question3,
+        'question4': $scope.question4,
+        'firstName': $scope.firstName,
+        'lastName': $scope.lastName,
+        'address': $scope.address
+
+      })
+
+      $scope.surveyCode = "";
+      $scope.question1 = "";
+      $scope.question2 = "";
+      $scope.question3 = "";
+      $scope.question4 = "";
+      $scope.firstName = "";
+      $scope.lastName = "";
+      $scope.address = "";
+
+    }
+
+
+
   });
