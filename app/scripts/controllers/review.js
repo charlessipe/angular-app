be55cde0eceb3f879f476a8abac5d14260724965
Charles Sipe <csipe84@gmail.com>
2016-08-15 17:20:21 -0700

    Require correct code to push survey values to Firebase

--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -14,29 +14,35 @@ angular.module('angularAppApp')
     var surveyResponses = firebase.database().ref();
 
     $scope.getFormValues = function(){
+      if($scope.surveyCode == "popcorn"){
+        surveyResponses.child("businessProfile").child("signature-pointe").child("surveyAnswers").push({
+          'name': "Signature Pointe",
+          'code': $scope.surveyCode,
+          'question1': $scope.question1,
+          'question2': $scope.question2,
+          'question3': $scope.question3,
+          'question4': $scope.question4,
+          'firstName': $scope.firstName,
+          'lastName': $scope.lastName,
+          'address': $scope.address
 
-      surveyResponses.child("businessProfile").child("signature-pointe").child("surveyAnswers").push({
-        'name': "Signature Pointe",
-        'code': $scope.surveyCode,
-        'question1': $scope.question1,
-        'question2': $scope.question2,
-        'question3': $scope.question3,
-        'question4': $scope.question4,
-        'firstName': $scope.firstName,
-        'lastName': $scope.lastName,
-        'address': $scope.address
+        })
 
-      })
+        $scope.surveyCode = "";
+        $scope.question1 = "";
+        $scope.question2 = "";
+        $scope.question3 = "";
+        $scope.question4 = "";
+        $scope.firstName = "";
+        $scope.lastName = "";
+        $scope.address = "";
 
-      $scope.surveyCode = "";
-      $scope.question1 = "";
-      $scope.question2 = "";
-      $scope.question3 = "";
-      $scope.question4 = "";
-      $scope.firstName = "";
-      $scope.lastName = "";
-      $scope.address = "";
-
+        $scope.surveySubmitSuccess = true;
+        $scope.wrongSurveyCode = false;
+      }
+      else {
+        $scope.wrongSurveyCode = true;
+      }
     }
 
 
